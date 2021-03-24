68750efeb5da9cd1e4658c6027c3fe70c3a8492f
pattonw <pattonw@hhmi.org>
2021-03-24 13:09:56 -0400

    remove depricated helper function

--- dashboard/dacapo/helpers.py
+++ dashboard/dacapo/helpers.py
@@ -1,5 +1,3 @@
-import toml
-
 from dashboard.db import get_db
 
 import hashlib
@@ -23,14 +21,3 @@ def get_checklist_data():
         "users": [user["username"] for user in db.users.find({})],
     }
     return context
-
-
-def parse_config_form(form):
-    data = {"name": form["name"]}
-    parsed = toml.loads(form["content"])
-    assert "name" not in parsed
-    data.update(parsed)
-
-    id_num = hashlib.md5(toml.dumps(data).encode("utf-8")).hexdigest()
-    data["id"] = id_num
-    return data
