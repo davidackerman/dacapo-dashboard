b7dc2e51e8f6a231f7115f9a3c7548be63628812
David Ackerman <ackermand@janelia.hhmi.org>
2021-05-26 17:48:07 -0400

    made tasks, optimizers etc copyable and viewable as tooltips

--- dashboard/dacapo/helpers.py
+++ dashboard/dacapo/helpers.py
@@ -1,4 +1,5 @@
 from dashboard.db import get_db
+from bson.json_util import dumps
 
 import hashlib
 
@@ -6,16 +7,16 @@ import hashlib
 def get_checklist_data():
     db = get_db()
     context = {
-        "tasks": [(task["id"], task.get("name", "UGH")) for task in db.tasks.find({})],
+        "tasks": [(task["id"], task.get("name", "UGH"), dumps(task, indent=2)) for task in db.tasks.find({})],
         "datasets": [
-            (dataset["id"], dataset.get("name", "UGH"))
+            (dataset["id"], dataset.get("name", "UGH"), dumps(dataset, indent=2))
             for dataset in db.datasets.find({})
         ],
         "models": [
-            (model["id"], model.get("name", "UGH")) for model in db.models.find({})
+            (model["id"], model.get("name", "UGH"), dumps(model, indent=2)) for model in db.models.find({})
         ],
         "optimizers": [
-            (optimizer["id"], optimizer.get("name", "UGH"))
+            (optimizer["id"], optimizer.get("name", "UGH"), dumps(optimizer, indent=2))
             for optimizer in db.optimizers.find({})
         ],
         "users": [user["username"] for user in db.users.find({})],
