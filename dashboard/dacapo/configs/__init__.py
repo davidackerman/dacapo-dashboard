700a24be40683bd692d4964b7ae3541a7149c842
pattonw <pattonw@hhmi.org>
2022-04-27 10:58:04 -0400

    make config global variables available in __init__

--- dashboard/dacapo/configs/__init__.py
+++ dashboard/dacapo/configs/__init__.py
@@ -1,3 +1,3 @@
 from .blue_print import bp
 
-from .routes import config_json, all_json, config_form, all_form
+from .routes import config_json, all_json, config_form, all_form, CONFIGURABLE_FIELDS, CONFIGURABLES
