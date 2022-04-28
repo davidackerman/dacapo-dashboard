9e3913b7598da45fdd67c1226de7e119149cac3c
David Ackerman <ackermand@janelia.hhmi.org>
2022-04-28 17:45:55 -0400

    update authorization to include registration of nextflow info

--- dashboard/auth.py
+++ dashboard/auth.py
@@ -10,7 +10,11 @@ from flask import (
     session,
     url_for,
 )
+from flask_login import login_manager
+import flask_login
+from flask_login.utils import login_required
 from werkzeug.security import check_password_hash, generate_password_hash
+from wtforms.fields.simple import TextAreaField
 from dashboard.authservice import create_auth_service
 
 from dashboard.stores import get_stores
@@ -22,99 +26,139 @@ from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from wtforms import StringField, PasswordField
 
+from dashboard.nextflow import Nextflow
+
 bp = Blueprint("auth", __name__, url_prefix="/auth")
-
-# TODO: Where to put users now
-@bp.route("/register", methods=("GET", "POST"))
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        db = get_stores()
-        error = None
-
-        if not username:
-            error = "Username is required."
-        elif not password:
-            error = "Password is required."
-        elif db.users.find_one({"username": username}) is not None:
-            error = "Username already exists."
-
-        if error is None:
-            db.users.insert_one(
-                {"username": username, "password": generate_password_hash(password)}
-            )
-            return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template("auth/register.html")
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username',
-                           validators=[DataRequired('Username is required')])
-    password = PasswordField('password')
+    username = StringField(
+        "username", validators=[DataRequired("Username is required")]
+    )
+    password = PasswordField(
+        "password", validators=[DataRequired("Password is required")]
+    )
 
 
-@bp.route('/login_form', methods=['GET', 'POST'])
-def login_form():
-    next_page_param = request.args.get('next')
-    form = LoginForm(csrf_enabled=True)
-    return render_template('auth/login.html',
-                           form=form,
-                           next=next_page_param if next_page_param else '')
+class RegisterForm(FlaskForm):
+    username = StringField(
+        "username", validators=[DataRequired("Username is required")]
+    )
+    password = PasswordField(
+        "password", validators=[DataRequired("Password is required")]
+    )
+    api_token = StringField(
+        "api_token", validators=[DataRequired("API token is required")],
+    )
+    ssh_key = TextAreaField("ssh_key", validators=[DataRequired("SSH key is required")])
 
 
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.args.get('next'):
-        next_page = request.args.get('next')
-    elif request.form.get('next'):
-        next_page = request.form.get('next')
+# TODO: Where to put users now
+
+
+@bp.route("/register_form", methods=["GET", "POST"])
+def register_form():
+    next_page = request.args.get("next")
+    print("register_form", next_page)
+    form = RegisterForm(csrf_enabled=True)
+    if not form.validate_on_submit():
+        return render_template("auth/register.html", form=form, next=next_page), 401
+    return render_template(
+        "auth/register.html", form=form, next=next_page if next_page else ""
+    )
+
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    if request.args.get("next"):
+        next_page = request.args.get("next")
+    elif request.form.get("next"):
+        next_page = request.form.get("next")
     else:
-        next_page = url_for('main')
+        next_page = url_for("main")
 
+    form = RegisterForm(csrf_enabled=True)
+    auth_service = create_auth_service()
+    username = form.username.data
+    user_credentials = {"username": username, "password": form.password.data}
+    api_token = form.api_token.data
+    ssh_key = form.ssh_key.data
+    authResponse = auth_service.authenticate_user(user_credentials)
+    error_message = ""
+    if authResponse.status_code == 200:
+        user_info = {"name": username, "api_token": api_token}
+        nextflow = Nextflow(user_info, ssh_key)
+        if nextflow.verified:
+            config_store = get_stores().config
+            config_store.store_user_info(user_info)
+            auth_service.login(user_credentials)
+            # setup_compute_environment...
+            return redirect(next_page)
+        else:
+            error_message = nextflow.error_message
+    else:
+        error_message = "Invalid Username or Password"
+
+    return render_template(
+        "auth/register.html", form=form, next=next_page, error_message=error_message
+    )
+
+
+@bp.route("/login_form", methods=["GET", "POST"])
+def login_form():
+    next_page_param = request.args.get("next")
     form = LoginForm(csrf_enabled=True)
     if not form.validate_on_submit():
         form = LoginForm(csrf_enabled=True)
-        return render_template('auth/login.html',
-                               form=form,
-                               next=next_page if next_page else ''), 401
+        return render_template("auth/login.html", form=form, next=next_page_param), 401
+    return render_template(
+        "auth/login.html", form=form, next=next_page_param if next_page_param else ""
+    )
+
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.args.get("next"):
+        next_page = request.args.get("next")
+    elif request.form.get("next"):
+        next_page = request.form.get("next")
+    else:
+        next_page = url_for("main")
+
+    if not next_page:
+        next_page = ""
+
+    form = LoginForm(csrf_enabled=True)
+    # if not form.validate_on_submit():
+    #     form = LoginForm(csrf_enabled=True)
+    #     return render_template('auth/login.html',
+    #                            form=form,
+    #                            next=next), 401
 
     # execute login
     auth_service = create_auth_service()
-    authenticated = auth_service.authenticate(
-        {'username': form.username.data, 'password': form.password.data})
+    user_credentials = {"username": form.username.data, "password": form.password.data}
+    authResponse = auth_service.authenticate_user(user_credentials)
 
-    if authenticated:
-        return redirect(next_page)
+    if authResponse.status_code == 200:
+        config_store = get_stores().config
+        user_info = config_store.retrieve_user_info(user_credentials["username"])
+        if user_info:
+            logged_in = auth_service.login(user_credentials)
+            if logged_in:
+                return redirect(next_page)
+        else:
+            return redirect(url_for("auth.register_form", next=next_page))
     else:
-        return render_template('auth/login.html',
-                               form=form,
-                               next=next_page if next_page else ''), 401
-
-# def login():
-#     if request.method == "POST":
-#         username = request.form["username"]
-#         password = request.form["password"]
-#         db = get_stores()
-#         error = None
-#         user = db.users.find_one({"username": username})
-
-#         if user is None:
-#             error = "Incorrect username."
-#         elif not check_password_hash(user["password"], password):
-#             error = "Incorrect password."
-
-#         if error is None:
-#             session.clear()
-#             session["user_id"] = str(user["_id"])
-#             return redirect(url_for("dacapo.get_results"))
-
-#         flash(error)
-
-#     return render_template("auth/login.html")
+        return (
+            render_template(
+                "auth/login.html",
+                form=form,
+                next=next_page,
+                error_message="Invalid Username or Password",
+            ),
+            401,
+        )
 
 
 @bp.before_app_request
